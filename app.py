8e90750d1af8950cf6ba5f9164907ef93d6be27c
AshutoshJ-99 <ashujadhav1998@gmail.com>
2025-07-29 20:31:54 +0530

    Update message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on EKS!"
+    return "Hello from updated Flask on EKS!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
